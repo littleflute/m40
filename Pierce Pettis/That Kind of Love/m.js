e1b820445c8cb6c1085ea9e979cad9704388edaf
littleflute <13699175164@sina.cn>
2017-07-10 15:05:51 -0500

    Add files via upload

---
+++ Pierce Pettis/That Kind of Love/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Nothing But the Wind.mp3";		
+f[2]	= "02 I Am Nothing.mp3";		
+f[3]	= "05 Lion's Eye.mp3";		
+f[4]	= "06 You Did That for Me.mp3";		
+f[5]	= "07 That Kind of Love.mp3";		
+f[6]	= "08 Talk Memphis.mp3";		
+f[7]	= "09 To Dance.mp3";		
+f[8]	= "10 Hallelujah Song.mp3";		
+f[9]	= "11 Pastures of Plenty.mp3";		
+f[10]	= "12 Something for the Pain.mp3";		
+f[11]	= "Farewell.mp3";		
+f[12]	= "Veracruz.mp3";		
